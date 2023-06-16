1a021b3f066c49dc71d460b26dffb8046958e639
Emerson Rafael <emersona7x@hotmail.com>
2023-06-16 03:38:38 -0300

    [ADD] ADICIONANDO FUNÇÃO PARA LER DADOS DA ÚLTIMA PARTIÇÃO DE UM PARQUET PARTICIONADO

--- etl_functions.py
+++ etl_functions.py
@@ -30,6 +30,21 @@ class Read_Files():
     @staticmethod
     def show_dataframe(df, num_rows_to_display=5):
 
+        """
+
+            FUNÇÃO PARA VISUALIZAR UM DATAFRAME
+            FUNCIONA PARA DATAFRAMES PANDAS
+            E DATAFRAMES SPARK
+
+            TAMBÉM MOSTRA O TIPAGEM DOS DADOS
+
+            # Arguments
+                df                     - Required: Dataframe a ser visualizado (DataFrame)
+                num_rows_to_display    - Optional: Quantidade de linhas
+                                                   para visualizar (Integer)
+
+        """
+
         # VERIFICANDO SE O DATAFRAME É PANDAS
         if isinstance(df, pd.DataFrame):
 
@@ -58,6 +73,10 @@ class Read_Files():
 
         """
 
+        FUNÇÃO PARA REALIZAR LEITURA DE DADOS
+        DE UM EXCEL
+        USANDO PANDAS DATAFRAME
+
         # Arguments
             data_dir                    - Required: Diretório do arquivo excel a ser lido (Path)
             sheet_name                  - Optional: Aba do arquivo excel a ser lido (String)
@@ -68,7 +87,7 @@ class Read_Files():
             schema                      - Optional: Tipagem de cada coluna. É um dict,
                                                     no qual a chave representa o
                                                     nome da coluna e o
-                                                    valor seu tipo, respectivamente.
+                                                    valor seu tipo, respectivamente. (None | StructType)
 
         # Returns
             df                          - Required: Dados obtidos (Pandas DataFrame)
@@ -118,6 +137,10 @@ class Read_Files():
 
         """
 
+        FUNÇÃO PARA REALIZAR LEITURA DE DADOS
+        DE UM CSV
+        USANDO PANDAS DATAFRAME
+
         # Arguments
             data_dir                    - Required: Diretório do arquivo excel a ser lido (Path)
             sep                         - Optional: Separador dos dados (String)
@@ -166,6 +189,27 @@ class Read_Files():
                        encoding: str = "latin1",
                        schema=None):
 
+        """
+
+        FUNÇÃO PARA REALIZAR LEITURA DE DADOS
+        DE UM CSV
+        USANDO SPARK DATAFRAME
+
+        # Arguments
+            data_dir                    - Required: Diretório do arquivo excel a ser lido (Path)
+            header                      - Optional: Se os dados possuem cabeçalho (Boolean)
+            sep                         - Optional: Separador dos dados (String)
+            encoding                    - Optional: Qual o formato de codificação dos dados.
+            schema                      - Optional: Tipagem de cada coluna. É um dict,
+                                                    no qual a chave representa o
+                                                    nome da coluna e o
+                                                    valor seu tipo, respectivamente.
+
+        # Returns
+            df                          - Required: Dados obtidos (Spark DataFrame)
+
+        """
+
         # INICIALIZANDO DATAFRAME VAZIO
         df_csv = pd.DataFrame()
 
@@ -208,6 +252,25 @@ class Read_Files():
                            header: bool = True,
                            schema=None):
 
+        """
+
+        FUNÇÃO PARA REALIZAR LEITURA DE DADOS
+        DE UM PARQUET
+        USANDO SPARK DATAFRAME
+
+        # Arguments
+            data_dir                    - Required: Diretório do arquivo excel a ser lido (Path)
+            header                      - Optional: Se os dados possuem cabeçalho (Boolean)
+            schema                      - Optional: Tipagem de cada coluna. É um dict,
+                                                    no qual a chave representa o
+                                                    nome da coluna e o
+                                                    valor seu tipo, respectivamente.
+
+        # Returns
+            df                          - Required: Dados obtidos (Spark DataFrame)
+
+        """
+
         # INICIALIZANDO DATAFRAME VAZIO
         df_parquet = pd.DataFrame()
 
@@ -232,6 +295,81 @@ class Read_Files():
                     spark.read.option("header", header)
                         .schema(schema)
                         .parquet(data_dir)
+                )
+
+            logger.info("LEITURA REALIZADA COM SUCESSO")
+
+        except Exception as ex:
+            logger.error("ERRO NA FUNÇÃO: {} - {}".format(stack()[0][3], ex))
+
+        return df_parquet
+
+    @staticmethod
+    def spark_read_parquet_last_partition(data_dir: typing.Union[str, os.PathLike],
+                                          header: bool = True,
+                                          schema=None):
+
+        """
+
+        FUNÇÃO PARA REALIZAR LEITURA DE DADOS
+        DE UM PARQUET PARTICIONANDO
+        USANDO SPARK DATAFRAME
+
+        RETORNA A ÚLTIMA PARTIÇÃO
+
+        # Arguments
+            data_dir                    - Required: Diretório do arquivo excel a ser lido (Path)
+            header                      - Optional: Se os dados possuem cabeçalho (Boolean)
+            schema                      - Optional: Tipagem de cada coluna. É um dict,
+                                                    no qual a chave representa o
+                                                    nome da coluna e o
+                                                    valor seu tipo, respectivamente.
+
+        # Returns
+            df                          - Required: Dados obtidos (Spark DataFrame)
+
+        """
+
+        # INICIALIZANDO DATAFRAME VAZIO
+        df_parquet = pd.DataFrame()
+
+        logger.info("INICIANDO A LEITURA DOS DADOS UTILIZANDO SPARK PARQUET")
+
+        try:
+
+            # OBTÉM A LISTA DE DIRETÓRIOS DE PARTIÇÃO
+            dir_partitions = spark._jvm.org.apache.hadoop.fs.Path(data_dir) \
+                .getFileSystem(spark._jsc.hadoopConfiguration()) \
+                .listStatus(spark._jvm.org.apache.hadoop.fs.Path(data_dir))
+
+            logger.info("DIRETÓRIOS PARTICIONADOS: {}".format(dir_partitions))
+
+            # ORDENANDO OS DIRETÓRIOS DE PARTIÇÃO PELA ORDEM LEXICOGRÁFICA REVERSA
+            dir_partitions = sorted(dir_partitions, key=lambda x: x.getPath().toString(), reverse=True)
+
+            # OBTÉM O PATH PARA A ÚLTIMA PARTIÇÃO
+            dir_last_partition = dir_partitions[0].getPath().toString()
+
+            logger.info("DIRETÓRIO DE ÚLTIMA PARTIÇÃO: {}".format(dir_last_partition))
+
+            # VERIFICANDO SE UM SCHEMA FOI DEFINIDO
+            if schema is None:
+
+                logger.info("REALIZANDO LEITURA SEM SCHEMA DEFINIDO")
+
+                df_parquet = (
+                    spark.read.option("header", header)
+                        .parquet(dir_last_partition)
+                )
+
+            elif isinstance(schema, [StructType, str]):
+
+                logger.info("REALIZANDO LEITURA COM SCHEMA DEFINIDO: {}".format(schema))
+
+                df_parquet = (
+                    spark.read.option("header", header)
+                        .schema(schema)
+                        .parquet(dir_last_partition)
                 )
 
             logger.info("LEITURA REALIZADA COM SUCESSO")
--- main_etl.py
+++ main_etl.py
@@ -3,6 +3,7 @@ from etl_functions import Read_Files
 data_dir_excel = "DATA/BASE_COM_CEP.xlsx"
 data_dir_csv = "DATA/despachantes.csv"
 data_dir_parquet = "result_parquet/data=01%2F12%2F2022"
+data_dir_parquet_partitions = "result_parquet/"
 
 # INICIALIZANDO A CLASSE DO READ FILES
 read_files_functions = Read_Files()
@@ -25,4 +26,9 @@ print("-"*50)
 # REALIZANDO A LEITURA COM SPARK PARQUET
 df_spark_parquet = read_files_functions.spark_read_parquet(data_dir_parquet)
 read_files_functions.show_dataframe(df=df_spark_parquet, num_rows_to_display=10)
+print("-"*50)
+
+# REALIZANDO A LEITURA DA ÚLTIMA PARTIÇÃO COM SPARK PARQUET
+df_spark_parquet_last_partiton = read_files_functions.spark_read_parquet_last_partition(data_dir_parquet_partitions)
+read_files_functions.show_dataframe(df=df_spark_parquet_last_partiton, num_rows_to_display=10)
 print("-"*50)
